588184f0cd68abc5f95f7da9655df651e4b69181
Vindo Kountur <vindokountur@gmail.com>
2020-03-11 19:09:23 +0800

    Updates

---
+++ precache-manifest.cf032aa484e627e7e590a2c86daefaab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c902204a4997c2880bf4e61fb0f44dad",
+    "url": "/qod/index.html"
+  },
+  {
+    "revision": "c45458f3938c14be0494",
+    "url": "/qod/static/css/main.e213682b.chunk.css"
+  },
+  {
+    "revision": "12f87ad25f9d66b1a097",
+    "url": "/qod/static/js/2.82e091ef.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/qod/static/js/2.82e091ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45458f3938c14be0494",
+    "url": "/qod/static/js/main.f72b35c8.chunk.js"
+  },
+  {
+    "revision": "2d9625c3702056884ea2",
+    "url": "/qod/static/js/runtime-main.63d1fa0f.js"
+  }
+]);
